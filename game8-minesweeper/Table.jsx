b7dbaf273fa9cd5403ba7749ba0de49e2a3fea22
HurGeon <geon.hur@meshkorea.net>
2022-04-12 22:58:28 +0900

    지뢰찾기. context API, createContext, Provider, useContext

---
+++ game8-minesweeper/Table.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import Tr from "./Tr";
+import {TableContext} from "./MineSweeper";
+
+const Table = () => {
+    const {tableData} = useContext(TableContext);
+    return (
+        <table>
+            {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex={i}/>)}
+        </table>
+    )
+}
+
+export default Table;
